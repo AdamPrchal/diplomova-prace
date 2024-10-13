ab10b7092364c6e23f380b7e3ec7368c85211190
Adam Prchal <adamprchal@Mac.lan>
2024-10-13 12:21:57 +0200

    improving latex setup

--- latex/watch.sh
+++ latex/watch.sh
@@ -1,9 +1,10 @@
+latexmk -xelatex main.tex
 fswatch -e ".*" -i ".*/[^.]*\\.tex$" -i ".*/zdroje\\.csv$" -0 . | while read -d "" event
 do
     if [[ "$event" =~ \.tex$ ]]; then
-        xelatex main.tex
+        latexmk -xelatex main.tex
     elif [[ "$event" == *zdroje.csv ]]; then
         python3 csv_to_lit.py
-        xelatex main.tex
+        latexmk -xelatex main.tex
     fi
 done
